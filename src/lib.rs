f430c5184cb0db19428361e490790eaacda22a89
chavicoski <alvaro.lopez.chilet@gmail.com>
2023-06-02 18:29:45 +0200

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,188 @@
+mod utils;
+
+use std::cmp::Ordering;
+use wasm_bindgen::prelude::*;
+
+// To access the JS Math.random
+extern crate js_sys;
+
+// To use single-bit encoding for the universe Cells
+extern crate fixedbitset;
+use fixedbitset::FixedBitSet;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+#[allow(unused_macros)]
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: FixedBitSet,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new() -> Universe {
+        // Enable better error messages in case of panic
+        utils::set_panic_hook();
+
+        // Set the universe dimensions
+        let width = 64;
+        let height = 64;
+
+        // Create the universe data structure
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        // Set the universe state randomly
+        for i in 0..size {
+            cells.set(i, js_sys::Math::random() < 0.5);
+        }
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn size(&self) -> usize {
+        (self.width * self.height) as usize
+    }
+
+    fn reset_with_size(&mut self, new_size: usize) {
+        let curr_size = self.size();
+        match new_size.cmp(&curr_size) {
+            Ordering::Greater => {
+                self.cells.grow(new_size);
+                self.cells.clear();
+            }
+            Ordering::Less => {
+                self.cells = FixedBitSet::with_capacity(new_size);
+                self.cells.clear();
+            }
+            Ordering::Equal => self.cells.clear(),
+        }
+    }
+
+    /// Set the width of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        let new_size = (width * self.height) as usize;
+        self.reset_with_size(new_size);
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        let new_size = (self.width * height) as usize;
+        self.reset_with_size(new_size);
+    }
+
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
+    }
+
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                next.set(
+                    idx,
+                    match (cell, live_neighbors) {
+                        (true, x) if x < 2 => false,
+                        (true, 2) | (true, 3) => true,
+                        (true, x) if x > 3 => false,
+                        (false, 3) => true,
+                        (otherwise, _) => otherwise,
+                    },
+                );
+            }
+        }
+
+        self.cells = next;
+    }
+
+    pub fn toggle_cell(&mut self, row: u32, column: u32) {
+        let idx = self.get_index(row, column);
+        self.cells.toggle(idx);
+    }
+}
+
+impl Universe {
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &FixedBitSet {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells.set(idx, true);
+        }
+    }
+}
+
+impl Default for Universe {
+    fn default() -> Self {
+        Self::new()
+    }
+}

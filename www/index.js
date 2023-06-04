5d24a9d7633d72e6a141bcb42cf145215f4ba4e8
chavicoski <alvaro.lopez.chilet@gmail.com>
2023-06-04 08:35:48 +0200

    Added fps counter

--- www/index.js
+++ www/index.js
@@ -69,11 +69,17 @@ clearButton.addEventListener("click", () => {
 
 // Animation loop
 const renderLoop = () => {
+  // Keep track of the FPS metrics
+  fps.render();
+
+  // Compute the next state of the universe
   universe.update();
 
+  // Draw the universe new state
   drawGrid();
   drawCells();
 
+  // Queue the next frame calculation
   animationId = requestAnimationFrame(renderLoop);
 };
 
@@ -157,6 +163,49 @@ canvas.addEventListener("click", (event) => {
   drawGrid();
   drawCells();
 });
+
+const fps = new (class {
+  constructor() {
+    this.fps = document.getElementById("fps");
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // Convert the delta time since the last frame render into a measure
+    // of frames per second
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = (1 / delta) * 1000;
+
+    // Save only the latest 100 timings
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    // Find the max, min, and mean of our 100 latest timings
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    // Render the statistics
+    this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+  }
+})();
 
 // Make the initial call to the animation loop
 play();
